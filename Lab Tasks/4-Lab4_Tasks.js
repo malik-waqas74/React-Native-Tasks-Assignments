483a4b09525f4cb57971392c8be717828f054436
Malik Waqas <109264486+malik-waqas74@users.noreply.github.com>
2022-09-25 11:38:39 +0500

    Update 4-Lab4_Tasks.js

--- Lab Tasks/4-Lab4_Tasks.js
+++ Lab Tasks/4-Lab4_Tasks.js
@@ -65,7 +65,7 @@ console.log(`total Sum=${sum}`);
 Write a JavaScript that calculates the squares and cubes of the numbers from 0 to 10.
  */
 
-var squareAndCubes = (...args) => {
+var squareAndCubes = () => {
     let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     let squares = num.map((na) => {
